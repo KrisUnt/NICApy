1eace0a896247884c2aa6b25653c5177fcf5616e
Kris U <kris.unterhauser@hotmail.com>
2021-12-13 20:54:32 +0100

    Delete data_sharing_objects directory

--- data_sharing_objects/hdr_class.py
+++
@@ -1,8 +0,0 @@
-# Hdr class as dict
-class Hdr(dict):
-    '''
-    Class allowing to create a Hdr object of type dict in order to store hdr related information
-    Inherited from dict.
-    '''
-    def __init__(self):
-        dict.__init__(self)
--- data_sharing_objects/xdf_class.py
+++
@@ -1,8 +0,0 @@
-# Xdf class as dict
-class Xdf(dict):
-    '''
-    Class allowing to create a Xdf object of type dict in order to store xdf related information
-    Inherited from dict.
-    '''
-    def __init__(self):
-        dict.__init__(self)
